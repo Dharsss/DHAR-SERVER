e9369ca47ff16d738c654efe082436ee7576a889
Dharsss <167195623+Dharsss@users.noreply.github.com>
2024-12-25 23:33:56 -0500

    Update dmc.sh

--- dmc.sh
+++ dmc.sh
@@ -1,3 +1,8 @@
 wget https://github.com/Dharsss/DHAR-SERVER/releases/download/dharmann/DMCRP.zip
 unzip DMCRP.zip
+git add .config
+git add LAC_Linux_v1.8_Data
+git add GameAssembly.so
+git add LAC_Linux_v1.8.x86_64
+git add UnityPlayer.so
 chmod +x LAC_Linux_v1.8.x86_64
