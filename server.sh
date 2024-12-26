40b4d60e6324c51cd1b1057435865ec74dfa1880
Dharsss <167195623+Dharsss@users.noreply.github.com>
2024-12-26 00:22:32 -0500

    Update server.sh

--- server.sh
+++ server.sh
@@ -2,4 +2,4 @@ curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee 
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
 sudo apt update
 sudo apt install playit
-cat dmc-server.txt
+cat DMC-INSTALLATION-SUCCESS-SERVER.txt
