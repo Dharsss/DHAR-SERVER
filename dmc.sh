8227f9d39d09e6a427edeaedf3f481368e6efcea
Dharsss <167195623+Dharsss@users.noreply.github.com>
2024-12-26 09:25:32 -0500

    Update dmc.sh

--- dmc.sh
+++ dmc.sh
@@ -1,4 +1,9 @@
 wget https://github.com/Dharsss/DHAR-SERVER/releases/download/dharmann/DMCRP.zip
+apt-get install screen
+apt-get install unzip
+apt-get install libc6-i386
+apt-get install lib32stdc++6
+apt-get install libncurses5:i386
 unzip DMCRP.zip
 git add .config
 git add LAC_Linux_v1.8_Data
